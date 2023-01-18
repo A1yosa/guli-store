15e732e0e38fd744821cdf269f97637567465728
Alohaaa <19200135221@post.usts.edu.cn>
2023-01-18 20:45:27 +0800

    项目初始结构创建

---
+++ gulistore-product/src/test/java/com/jay/gulistore/product/GulistoreProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.gulistore.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulistoreProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
